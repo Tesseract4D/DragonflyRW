659c37a5f1c3529029c8c03744e644f6b1a8d0c9
梁弘毅 <55395193+tesseract4d@users.noreply.github.com>
2024-11-24 16:08:15 +0800

    3

--- src/main/java/cn/tesseract/dragonfly/Dragonfly.java
+++ src/main/java/cn/tesseract/dragonfly/Dragonfly.java
@@ -1,14 +1,12 @@
 package cn.tesseract.dragonfly;
 
 import cn.tesseract.dragonfly.asm.HookClassTransformer;
-import com.android.tools.r8.D8;
 import org.apache.commons.io.FileUtils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +17,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class Dragonfly {
     public static HookClassTransformer classTransformer = new HookClassTransformer();
@@ -27,8 +26,8 @@ public class Dragonfly {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        File apkFile = new File(workDir, "mod/libs/base.apk"),
-                dexFile = new File(workDir, "mod/libs/classes.bak.dex"),
+        File apkFile = new File(workDir, "mod/libs/base.bak.apk"),
+                apkOut = new File(workDir, "mod/libs/base.apk"),
                 target = new File(workDir, "mod/libs/classes.bak.jar"),
                 mod = new File(workDir, "mod/build/libs/mod-1.0-SNAPSHOT.jar"),
                 out = new File(workDir, "mod/libs/classes.jar"),
@@ -68,10 +67,54 @@ public class Dragonfly {
             mergeJar(newJar, oldJar, existingEntries, hookContainers, true);
         }
 
-        D8.main(new String[]{out.toPath().toString(), "--min-api", "28", "--output", dexOut.getParent()});
+        //D8.main(new String[]{out.toPath().toString(), "--min-api", "28", "--output", dexOut.getParent()});
+
+        ZipInputStream s = new ZipInputStream(Files.newInputStream(apkFile.toPath()));
+        System.out.println(s.getNextEntry());
+        System.out.println(s.getNextEntry());
+        System.out.println(s.getNextEntry());
+FileUtils.copyFile();
     }
 
     public static void mergeJar(JarOutputStream newJar, ZipInputStream oldJar, Set<String> existingEntries, Set<String> hookContainers, boolean isTarget) throws IOException {
+        ZipEntry entry;
+        while ((entry = oldJar.getNextEntry()) != null) {
+            String name = entry.getName();
+            if (!isTarget && name.equals("META-INF/MANIFEST.MF")) {
+                String[] mfs = new String(readEntryBytes(oldJar)).split("\n");
+                for (String mf : mfs) {
+                    String s = mf.trim();
+                    int i = s.indexOf(':');
+                    if (i != -1 && i < s.length() - 1) {
+                        String key = s.substring(0, i).trim();
+                        String value = s.substring(i + 1).trim();
+                        if (key.equals("Hook-Container-Class"))
+                            hookContainers.add(value);
+                    }
+                }
+                continue;
+            }
+            if (existingEntries.contains(name)) {
+                continue;
+            }
+            existingEntries.add(name);
+            newJar.putNextEntry(new JarEntry(name));
+            byte[] data = readEntryBytes(oldJar);
+            if (name.endsWith(".class")) {
+                String className = name.substring(0, name.length() - 6).replace('/', '.');
+                if (hookContainers.contains(className)) {
+                    classTransformer.registerHookContainer(data);
+                } else {
+                    data = classTransformer.transform(className, data);
+                }
+            }
+            newJar.write(data);
+
+            newJar.closeEntry();
+        }
+    }
+
+    public static void mergeA(JarOutputStream newJar, ZipInputStream oldJar, Set<String> existingEntries, Set<String> hookContainers, boolean isTarget) throws IOException {
         ZipEntry entry;
         while ((entry = oldJar.getNextEntry()) != null) {
             String name = entry.getName();
