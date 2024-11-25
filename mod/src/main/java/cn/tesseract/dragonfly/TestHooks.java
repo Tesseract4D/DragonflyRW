08f0e48b90b27c97383c2e63d6fbfdb26de51f24
梁弘毅 <55395193+tesseract4d@users.noreply.github.com>
2024-11-25 23:02:30 +0800

    6

---
+++ mod/src/main/java/cn/tesseract/dragonfly/TestHooks.java
@@ -0,0 +1,21 @@
+package cn.tesseract.dragonfly;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import cn.tesseract.dragonfly.asm.Hook;
+import cn.tesseract.dragonfly.asm.ReturnCondition;
+import com.corrodinggames.rts.ally.game.units.class_415;
+import com.corrodinggames.rts.ally.gameFramework.class_328;
+import com.corrodinggames.rts.ally.gameFramework.class_998;
+import com.corrodinggames.rts.ally.gameFramework.f.class_1001;
+import com.corrodinggames.rts.ally.gameFramework.f.class_1002;
+import com.corrodinggames.rts.ally.gameFramework.f.class_960;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class TestHooks {
+
+
+}
